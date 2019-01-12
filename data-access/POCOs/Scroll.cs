827c3db1c65108a80150e206c709d7ece1f62aa8
Itay Zandbank <itay@chelem.co.il>
2019-01-13 01:31:19 +0200

    Start adding hte scrollversion

--- data-access/POCOs/Scroll.cs
+++ data-access/POCOs/Scroll.cs
@@ -29,4 +29,32 @@ namespace SQE.Backend.DataAccess.POCOs
         public string scroll_version_ids { get; set; }
         public string thumbnails { get; set; }
     }
+
+    public class RawUser
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+
+    public class ScrollVersion
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public List<int> scroll_version_ids;
+        public List<string> ThumbnailUrls;
+        // public User Owner;
+    }
+
+    internal class RawScrollVersion
+    {
+        // Taken from the query in line 1258 of scollery-cgi.pl
+        public string name { get; set; }
+        public int scroll_version_group_id { get; set; }
+        public string owner { get; set; }
+        public string shared { get; set; }
+        // Ignore number_of_versions - it's just the number of elements in scroll_version_ids
+        public string scroll_version_ids { get; set; }
+        public string thumbnails { get; set; }
+        // Ignore image_fragments - it's just number of elements in thumbnails
+    }
 }
