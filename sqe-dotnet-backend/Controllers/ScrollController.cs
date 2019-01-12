1fb5fe27562cd15e2c92b9573b018308cb2c1a57
Itay Zandbank <itay@chelem.co.il>
2019-01-12 23:57:05 +0200

    Add DTOs, three empty endpoints

---
+++ sqe-dotnet-backend/Controllers/ScrollController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SQE.Backend.DTOs;
+
+namespace sqe_dotnet_backend.Controllers
+{
+    [Route("api/scroll")]
+    [ApiController]
+    public class ScrollController : ControllerBase
+    {
+        [HttpGet("all")] // api/scroll/all
+        public async Task<ActionResult<IEnumerable<Scroll>>> ListAllScrolls()
+        {
+            return new List<Scroll>();
+        }
+
+        [HttpGet("versions/my")] // api/scroll/versions/all - return all of my scroll versions
+        public async Task<ActionResult<IEnumerable<ScrollVersion>>> ListMyScrllVersions()
+        {
+            return new List<ScrollVersion>();
+        }
+
+        [HttpGet("versions/{id}")] // api/scroll/versions/<version-id>
+        public async Task<ActionResult<ScrollVersion>> GetScrollVersion(int id)
+        {
+            return new ScrollVersion();
+        }
+    }
+}
---
+++ sqe-dotnet-backend/DTOs/Scrolls.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SQE.Backend.DTOs
+{
+    public class Scroll
+    {
+        public string name { get; set; }
+        public List<string> URLs { get; set; }
+        public List<int> scrollVersionIds { get; set; }
+        public int defaultScrollVersionId { get; set; }
+        public int numImageFragments;
+    }
+
+    public class User
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Permission
+    {
+        public bool canWrite { get; set; }
+        public bool canLock { get; set; }
+    }
+
+    public class Share
+    {
+        public User user { get; set; }
+        public Permission permission { get; set; }
+    }
+
+    public class ScrollVersion
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int versionId { get; set; }
+        public User owner { get; set; }
+        public Permission ownerPermission { get; set; }
+        public List<string> thumbnailUrls { get; set; }
+        public List<Share> shares { get; set; }
+
+        public int numOfArtefacts { get; set; }
+        public int numOfColumns { get; set; }
+        public int numOfFragments { get; set; }
+
+        public bool locked { get; set; }
+        public DateTime? lastEdit { get; set; }
+    }
+}
