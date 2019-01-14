d97850848cd458915e0475f6f9ea1bf0f21e4b24
Itay Zandbank <itay@chelem.co.il>
2019-01-14 23:06:30 +0200

    Start adding the scroll version requests

--- data-access/ScrollRepositry.cs
+++ data-access/ScrollRepositry.cs
@@ -15,7 +15,7 @@ namespace SQE.Backend.DataAccess
     {
         Task<IEnumerable<Scroll>> ListScrolls();
         // Task<IEnumerable<ScrollVersion>> ListMyScrollVersions();
-        // Task<ScrollVersion> GetScrollVersion(int id);
+        Task<IEnumerable<ScrollVersion>> GetScrollVersions(int masterVersionId);
     }
 
     public class ScrollRepository: BaseRepository, IScrollRepository
@@ -46,8 +46,37 @@ GROUP BY scroll_data.scroll_id
             using (var connection = OpenConnection())
             {
                 var results = await connection.QueryAsync<ListScrollQueryResponse>(sql);
-                return results.Select(raw => raw.CreateScroll());
+                return results.Select(raw => raw.CreateModel());
             }
+        }
+
+        public async Task<IEnumerable<ScrollVersion>> GetScrollVersions(int masterVersionId)
+        {
+            var sql = @"
+SELECT  sv2.scroll_version_id, 
+	JSON_OBJECT(""name"", user.user_name, ""user_id"", user.user_id) AS owner,
+    scroll_data.name AS name,
+    COUNT(DISTINCT artefact_position_owner.artefact_position_id) AS numOfArtefacts,
+    COUNT(DISTINCT col_data_owner.col_data_id) AS numOfColsFrags,
+    svg2.locked,
+    sv2.may_write AS can_write,
+    sv2.may_lock AS can_lock,
+    MAX(main_action.time) AS lastEdit
+FROM scroll_version_group AS svg1
+JOIN scroll_version AS sv1 ON svg1.scroll_version_group_id = sv1.scroll_version_group_id
+JOIN scroll_version_group AS svg2 ON svg2.scroll_id = svg1.scroll_id
+JOIN scroll_version AS sv2 ON svg2.scroll_version_group_id = sv2.scroll_version_group_id
+JOIN user ON user.user_id = sv2.user_id
+JOIN scroll_data_owner ON sv2.scroll_version_id = scroll_data_owner.scroll_version_id
+JOIN scroll_data USING(scroll_data_id)
+LEFT JOIN artefact_position_owner ON artefact_position_owner.scroll_version_id = sv2.scroll_version_id
+LEFT JOIN col_data_owner ON col_data_owner.scroll_version_id = sv2.scroll_version_id
+LEFT JOIN main_action ON main_action.scroll_version_id = sv2.scroll_version_id
+WHERE sv1.scroll_version_id = ?
+    AND(sv2.user_id = 1 OR sv2.user_id = ?)
+GROUP BY sv2.scroll_version_id";
+
+            return new List<ScrollVersion>();
         }
     }
 }
